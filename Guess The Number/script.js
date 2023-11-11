9afe7c467e1dd63d58f0db169728ea3f0a8c8941
saurav <brathwalsaurav99@gmail.com>
2023-11-12 00:08:59 +0530

    progress

--- Guess The Number/script.js
+++ Guess The Number/script.js
@@ -2,12 +2,16 @@ const user_input = document.querySelector("#my_input")
 const btn = document.querySelector('button')
 const guess_list = document.querySelector('#guess_list')
 const remaining_attempts = document.querySelector('#remaining')
+const message = document.querySelector('#msg')
+const user_status = document.querySelector('#user_status')
 
 
 // Generating a random number
-const random = Math.floor(Math.random() * 100 ) + 1
+const random_num = Math.floor(Math.random() * 100 ) + 1
+console.log(random_num);
 
-const prev_guess = []
+let prev_guess = []
+let guess_count = 1
 
 
 let want_to_play = true
@@ -18,7 +22,7 @@ if (want_to_play) {
         e.preventDefault()
         const user_guess = parseInt(user_input.value)
         validating(user_guess)
-        console.log(prev_guess);
+        // console.log(prev_guess);
     })
 }
 
@@ -34,14 +38,58 @@ function validating(guess){
     }else if(isNaN(guess)){
         alert("Enter a valid number 1-100")
     }else {
-        console.log(guess);
-        all_guess(guess)
+        console.log(user_input.value);
+        all_guess(guess)  // appending all the guesses to empty array
+        correct_guess(guess)
     }
 }
 
+function correct_guess(guess){
+    if (guess_count > 10) {
+        showmsg(`GAME OVER - Correct Number is ${random_num}`)
+        reset_values_msg(guess)
+        gameend()
+    }else {
+        checking(guess)
+        reset_values_msg(guess)
+    }
+}
+
+function checking(guess){
+    if(guess === random_num){
+        showmsg(`Your Guess is right. Won ----`)
+        gameend()
+
+    }else if(guess > random_num){
+        showmsg('Number is high -  Make another guess')
+    }else if (guess < random_num){
+        showmsg('Number is low - Make another guess')
+    }
+}
+
+function reset_values_msg(guess){
+    user_input.value = '';
+    guess_list.innerHTML += ` ${guess},`
+    guess_count++
+    remaining_attempts.innerHTML = `${11- guess_count}`
+
+}
+
+function showmsg(msg){
+    message.innerHTML = `${msg}`
+}
 
 
 // This function will add all inputs to array of prev_guess
 function all_guess(val){
     let hello = prev_guess.push(val)
+}
+
+
+// ending game - disabling userinput so that can't write - and adding button for new game
+function gameend(){
+    user_input.value = ''
+    user_input.setAttribute('disabled', '')
+    let bt = document.createElement('button')
+    bt.classList.add()
 }
